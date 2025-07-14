923ac2dc2ab40d9e1c5a1ffbc00104cf16a8c065
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-07-14 13:46:44 +0500

    Update auth.config.ts

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -1,19 +1,23 @@
-export const authConfig = {
+import type { AuthOptions, User } from "next-auth"
+import type { JWT } from "next-auth/jwt"
+import type { Session } from "next-auth"
+
+export const authConfig: Partial<AuthOptions> = {
   pages: {
     signIn: "/login",
   },
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user }: { token: JWT; user?: User }) {
       if (user) {
-        token.role = user.role;
-        token.id = user.id;
+        token.role = (user as any).role;
+        token.id = (user as any).id;
       }
       return token;
     },
-    async session({ session, token }) {
+    async session({ session, token }: { session: Session; token: JWT }) {
       if (session.user) {
-        session.user.role = token.role as string;
-        session.user.id = token.id as string;
+        (session.user as any).role = token.role;
+        (session.user as any).id = token.id;
       }
       return session;
     },
