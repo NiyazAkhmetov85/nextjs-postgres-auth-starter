f962bc545130501e34cdb9ff1e092186d1ce5b36
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-07-16 16:14:57 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
+// app/layout.tsx
 import './globals.css';
-
 import { GeistSans } from 'geist/font/sans';
+import { dir } from 'i18next';
+import { languages } from '../i18n/settings';
+import type { Metadata } from 'next';
 
 let title = 'Next.js + Postgres Auth Starter';
 let description =
   'This is a Next.js starter kit that uses NextAuth.js for simple email + password login and a Postgres database to persist the data.';
 
-export const metadata = {
+export const metadata: Metadata = {
   title,
   description,
   twitter: {
@@ -17,14 +20,21 @@ export const metadata = {
   metadataBase: new URL('https://nextjs-postgres-auth.vercel.app'),
 };
 
+export async function generateStaticParams() {
+  return languages.map((lng) => ({ lng }));
+}
+
 export default function RootLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { lng: string };
 }) {
   return (
-    <html lang="en">
+    <html lang={params.lng} dir={dir(params.lng)}>
       <body className={GeistSans.variable}>{children}</body>
     </html>
   );
 }
+
