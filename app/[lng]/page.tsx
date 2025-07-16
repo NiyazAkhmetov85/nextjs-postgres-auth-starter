e9d0836fc1e6ca7f837acd4991e111c24f6ca3d1
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-07-16 16:20:05 +0500

    Create page.tsx

---
+++ app/[lng]/page.tsx
@@ -0,0 +1,14 @@
+import initI18next from '../../i18n/server'
+import { useTranslation } from 'react-i18next'
+
+export default async function Page({ params }: { params: { lng: string } }) {
+  const i18n = await initI18next(params.lng)
+  const { t } = i18n
+
+  return (
+    <div>
+      <h1>{t('title')}</h1>
+      <p>{t('description')}</p>
+    </div>
+  )
+}
