98ceb56eda22cad66dde5016f6c9b6c6333ed908
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-07-17 13:43:21 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,20 @@
-import NextAuth from 'next-auth';
-import { authConfig } from 'app/auth.config';
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github"; // пример, замени на свои
+import { partialConfig } from "./app/auth.config";
 
-export default NextAuth(authConfig).auth;
+const fullConfig = {
+  ...partialConfig,
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    // другие провайдеры
+  ],
+};
+
+export default NextAuth(fullConfig).auth;
 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
