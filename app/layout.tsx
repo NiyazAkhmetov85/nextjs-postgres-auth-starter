270ac3955132383a60d4b7f223a16f992cb63cef
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-07-18 00:21:33 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { dir } from 'i18next';
 import { languages } from '../i18n/settings';
 import type { Metadata } from 'next';
 import { Suspense } from 'react';
-import { I18nProvider } from '@/i18n/I18nProvider'; // путь уточни по проекту
+// import { I18nProvider } from '@/i18n/I18nProvider'; // путь уточни по проекту
 
 export const metadata: Metadata = {
   title: 'Next.js + Postgres Auth Starter',
