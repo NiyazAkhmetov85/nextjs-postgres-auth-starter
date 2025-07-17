79de80d11ee2e1ffd73adf8b05e28b662b926ea0
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-07-17 08:37:01 +0500

    Update server.ts

--- i18n/server.ts
+++ i18n/server.ts
@@ -1,18 +1,23 @@
-import i18next from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import resourcesToBackend from 'i18next-resources-to-backend'
-import { getOptions } from './settings'
+// i18n/server.ts
 
-const initI18next = async (lng: string, ns?: string | string[]) => {
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
+import { getOptions } from './settings';
+
+const initI18next = async (lng: string, ns: string | string[] = 'common') => {
   if (!i18next.isInitialized) {
     await i18next
       .use(initReactI18next)
-      .use(resourcesToBackend((language: string, namespace: string) =>
-        import(`../public/locales/${language}/${namespace}.json`)
-      ))
-      .init(getOptions(lng, ns))
+      .use(
+        resourcesToBackend((language: string, namespace: string) =>
+          import(`../public/locales/${language}/${namespace}.json`)
+        )
+      )
+      .init(getOptions(lng, ns));
   }
-  return i18next
-}
 
-export default initI18next
+  return i18next;
+};
+
+export default initI18next;
