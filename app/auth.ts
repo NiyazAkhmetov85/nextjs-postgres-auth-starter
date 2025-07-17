047d8cc5de7245645c830a82b14dd3147323afd7
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-07-17 13:46:13 +0500

    Update auth.ts

--- app/auth.ts
+++ app/auth.ts
@@ -2,7 +2,8 @@ import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { compare } from 'bcrypt-ts';
 import { getUser } from 'app/db';
-import { authConfig } from 'app/auth.config';
+import { partialConfig } from 'app/auth.config';
+
 
 export const {
   handlers: { GET, POST },
