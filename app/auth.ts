0e88ec6fe476bf3284ac97529343aaf0790ba98c
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-07-17 13:50:11 +0500

    Update auth.ts

--- app/auth.ts
+++ app/auth.ts
@@ -4,21 +4,23 @@ import { compare } from 'bcrypt-ts';
 import { getUser } from 'app/db';
 import { partialConfig } from 'app/auth.config';
 
-
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
 } = NextAuth({
-  ...authConfig,
+  ...partialConfig, // ✅ исправлено с authConfig на partialConfig
   providers: [
     Credentials({
       async authorize({ email, password }: any) {
-        let user = await getUser(email);
-        if (user.length === 0) return null;
-        let passwordsMatch = await compare(password, user[0].password!);
-        if (passwordsMatch) return user[0] as any;
+        const user = await getUser(email);
+        if (!user || user.length === 0) return null;
+
+        const passwordsMatch = await compare(password, user[0].password!);
+        if (!passwordsMatch) return null;
+
+        return user[0] as any;
       },
     }),
   ],
